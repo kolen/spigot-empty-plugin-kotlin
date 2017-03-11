6c1baa5f4133521697da4a43e8b8ce674c18ba6c
kolen <incredible.angst@gmail.com>
2017-03-11 04:26:34 +0300

    Initial import

---
+++ src/main/java/org/lolka/test_plugin/TestPlugin.java
@@ -0,0 +1,16 @@
+package org.lolka.test_plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TestPlugin extends JavaPlugin
+{
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
